dfae6ff0a8d544b3a20ac7bd5831709b017036d0
JMJ Riyana <jeemariyana@gmail.com>
2020-11-04 00:06:40 +0530

    d

--- csv-download/src/index.js
+++ csv-download/src/index.js
@@ -1,31 +1,85 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 import CsvDownloader from 'react-csv-downloader';
 
-<CsvDownloader>
-  <button>Download</button>
-</CsvDownloader>
 
-const datas = [{
-  cell1: 'row 1 - cell 1',
-  cell2: 'row 1 - cell 2'
-}, {
-  cell1: 'row 2 - cell 1',
-  cell2: 'row 2 - cell 2'
-}];
+class ComponentToPrint extends React.Component {
+  render() {
+    const columns = [{
+      id: 'first',
+      displayName: 'First column'
+    }, {
+      id: 'second',
+      displayName: 'Second column'
+    }];
+   
+    const datas = [{
+      first: 'foo',
+      second: 'bar'
+    }, {
+      first: 'foobar',
+      second: 'foobar'
+    }];
+   
+    return (
+      <div>
+        <CsvDownloader
+          filename="myfile"
+          separator=";"
+          wrapColumnChar="'"
+          columns={columns}
+          datas={datas}
+          text="DOWNLOAD" />
+      </div>
+    );
+  }
+}
+  const Example = () => {
+    const datas = [{
+      cell1: 'row 1 - cell 1',
+      cell2: 'row 1 - cell 2'
+    }, {
+      cell1: 'row 2 - cell 1',
+      cell2: 'row 2 - cell 2'
+    }];
+    
+    
+    <CsvDownloader datas={datas}/>;
 
-<CsvDownloader datas={datas}/>
+    const columns = [{
+      id: 'cell1',
+      displayName: 'Cell 1'
+    }, {
+      id: 'cell2',
+      displayName: 'Cell 2'
+    }];
 
-const columns = [{
-  id: 'cell1',
-  displayName: 'Cell 1'
-}, {
-  id: 'cell2',
-  displayName: 'Cell 2'
-}];
+    <CsvDownloader columns={columns}/>;
+   
+    return (
+      <div>
+        <ComponentToPrint />
+        {/*<CsvDownloader>
+          <button>Download</button>
+        </CsvDownloader>*/}
+      </div>
+    );
+  };
 
-<CsvDownloader columns={columns}/> 
+render(<Example/>,document.querySelector("#root")
+);
+
+
+
+//******************************************************* */
+
+
+
+
+
+// content of myfile.csv
+// 'First column';'Second column'
+// 'foo';'bar'
+// 'foobar';'foobar'
