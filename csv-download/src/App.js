bee90727a1f5f62abab4d6e2095be7d293f735fa
JMJ Riyana <jeemariyana@gmail.com>
2020-11-04 10:45:51 +0530

    d

--- csv-download/src/App.js
+++ csv-download/src/App.js
@@ -1,22 +1,36 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import CsvDownloader from 'react-csv-downloader';
+
+const head = [{
+  id: 'first',
+  displayName: 'First column',
+}, {
+  id: 'second',
+  displayName: 'Second column',
+}];
+
+const datas = [{
+  first: 'foo',
+  second: 'bar',
+}, {
+  first: 'foobar',
+  second: 'foobar',
+}];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <CsvDownloader
+          filename="myfile"
+          separator=";"
+          columns={head}
+          datas={datas}
+        />
       </header>
     </div>
   );
