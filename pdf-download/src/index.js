c9e33c910b5f48b3ced8e02ef016fe6be7e522e0
JMJ Riyana <jeemariyana@gmail.com>
2020-11-08 21:18:53 +0530

    d

--- pdf-download/src/index.js
+++ pdf-download/src/index.js
@@ -156,7 +156,7 @@ const Example = () => {
   const componentRef = useRef();
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
-  })
+  });
  
   return (
     <div>
